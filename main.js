85afcdd37b18c09fa0a11e65b21ce161fc88f25b
jjmuvi95 <jjmuvi95@gmail.com>
2022-12-29 19:17:06 -0600

    Clase Lista de productos esta a medias

--- main.js
+++ main.js
@@ -1,8 +1,76 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+
+
 
 function toggleDesktopMenu (){
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu(){
+    const isCarritoClosed = aside.classList.contains('inactive');
+
+    if(!isCarritoClosed){
+        aside.classList.add('inactive')
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Televisión',
+    price: 250,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Computer',
+    price: 350,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+for (product of productList){
+   const prodcutCard = document.createElement('div');
+   prodcutCard.classList.add('product-card');
+
+   const img = document.createElement('img');
+   img.setAttribute('src', product.image);
+
+   const prodcutInfo = document.createElement('div');
+   prodcutInfo.classList.add('product-info');
+
+   const prodcutInfoDiv = document.createElement('div');
+
+   const prodcutPrice = document.createElement('p');
+   const prodcutName = document.createElement('p');
+
+
+
+
+
+
+}   
